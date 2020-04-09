352b8598e8a6214117ca59217c6b3c482aa88d2d
Andy Arias <bandy@uninorte.edu.co>
2020-04-09 17:16:01 -0500

    logi y medio registro hechos tambien se envia correo

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,25 +1,75 @@
-importScripts('https://www.gstatic.com/firebasejs/7.13.2/firebase-app.js')
+ 
 var firebaseConfig = {
-    apiKey: "AIzaSyCoIwutsqJDIzJGJRsJF_SJkDbDL3vVPbI",
-    authDomain: "front2parcial.firebaseapp.com",
-    databaseURL: "https://front2parcial.firebaseio.com",
-    projectId: "front2parcial",
-    storageBucket: "front2parcial.appspot.com",
-    messagingSenderId: "144302066826",
-    appId: "1:144302066826:web:018b648e767f1612a4f173"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+  apiKey: "AIzaSyB6SazgRS61wd-YQpReItp_Y-qtEw91NyY",
+  authDomain: "testonline-c7937.firebaseapp.com",
+  databaseURL: "https://testonline-c7937.firebaseio.com",
+  projectId: "testonline-c7937",
+  storageBucket: "testonline-c7937.appspot.com",
+  messagingSenderId: "472917740959",
+  appId: "1:472917740959:web:7344c16d012919793381aa",
+  measurementId: "G-4MQ5TWJRKC"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
   function registrar() {
-      let email=document.getElementById('email');
-      let password=document.getElementById('password');
-    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+    email = $("#email").val();
+            password = $("#password").val();
+      console.log(password);
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+    .then( function (user) {
+      firebase.auth().signInWithEmailAndPassword(email, password)
+      
+     .then( function () {
+
+      var user = firebase.auth().currentUser;
+
+      user.sendEmailVerification().then(function() {
+        window.location="admin.html";
+        // Email sent.
+      }).catch(function(error) {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+      });
+     }
+
+     ) 
+      .catch(function(error) {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+
+    });
+    })
+.catch(function(error) {
         // Handle Errors here.
         var errorCode = error.code;
         var errorMessage = error.message;
+        console.log( errorCode);
+        console.log(errorMessage);
         // ...
       });
+
   }
+  function session(){
+    firebase.auth().onAuthStateChanged(function(user) {
+      if (user) {
+       console.log("logeado");
+       
+
+      } else {
+        console.log("no logeado");
+      }
+    });
+  }
+  function logout(params) {
+    firebase.auth().signOut().then(function() {
+       console.log("se salio el perro");
+    }).catch(function(error) {
+      // An error happened.
+    });
+  }
+  session();
+  
  
  

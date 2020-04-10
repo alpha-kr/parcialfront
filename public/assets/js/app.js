051cf4fb34df205021b839af44cc9b97ce3df018
Josue D. Sarabia Cervantes <josuedavids@hotmail.com>
2020-04-09 20:39:08 -0500

    added login and register firebase

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,4 +1,4 @@
- 
+
 var firebaseConfig = {
   apiKey: "AIzaSyB6SazgRS61wd-YQpReItp_Y-qtEw91NyY",
   authDomain: "testonline-c7937.firebaseapp.com",
@@ -13,63 +13,109 @@ var firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
 
-  function registrar() {
-    email = $("#email").val();
-            password = $("#password").val();
-      console.log(password);
-    firebase.auth().createUserWithEmailAndPassword(email, password)
-    .then( function (user) {
+function registrar() {
+  email = $("#email").val();
+  password = $("#password").val();
+  console.log(password);
+  firebase.auth().createUserWithEmailAndPassword(email, password)
+    .then(function (user) {
       firebase.auth().signInWithEmailAndPassword(email, password)
-      
-     .then( function () {
 
-      var user = firebase.auth().currentUser;
+        .then(function () {
 
-      user.sendEmailVerification().then(function() {
-        window.location="admin.html";
-        // Email sent.
-      }).catch(function(error) {
-        var errorCode = error.code;
-        var errorMessage = error.message;
+          var user = firebase.auth().currentUser;
+
+          user.sendEmailVerification().then(function () {
+            window.location = "admin.html";
+            // Email sent.
+          }).catch(function (error) {
+            var errorCode = error.code;
+            var errorMessage = error.message;
+          });
+        }
+
+        )
+        .catch(function (error) {
+          var errorCode = error.code;
+          var errorMessage = error.message;
+
+        });
+
+        firebase.auth().onAuthStateChanged(function(user) {
+          if (user) {
+              uid = firebase.auth().currentUser.uid;
+              
+              firebase.database().ref("usuarios/" + uid).set({
+                  "Empresa": $("#nombreEmp").val(),
+                  "NombreR": $("#nombreRepLeg").val(),
+                  "Telefono": $("#telRepLeg").val(), 
+                  "documento": $("#numDoc").val(), 
+                  "tipoD": $("#tipoD").val(), 
+                  "tipoUser": true, 
+              });
+          } 
       });
-     }
-
-     ) 
-      .catch(function(error) {
-        var errorCode = error.code;
-        var errorMessage = error.message;
-
-    });
     })
-.catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        console.log( errorCode);
-        console.log(errorMessage);
-        // ...
+    .catch(function (error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log(errorCode);
+      console.log(errorMessage);
+      // ...
+    });
+
+}
+function session() {
+  firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+      console.log("logeado");
+
+
+    } else {
+      console.log("no logeado");
+    }
+  });
+}
+
+function iniciarSesion() {
+  email = $("#email").val();
+  password = $("#password").val();
+  firebase.auth().signInWithEmailAndPassword(email, password).catch(function (error) {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+
+  });
+  firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+      firebase.auth().currentUser.getIdToken().then(function (idToken) {
+        localStorage.auth = idToken;
+        localStorage.uid = firebase.auth().currentUser.uid;
       });
 
-  }
-  function session(){
-    firebase.auth().onAuthStateChanged(function(user) {
-      if (user) {
-       console.log("logeado");
-       
+      firebase.database().ref('usuarios/' + firebase.auth().currentUser.uid)
+        .on('value', function(snapshot) {
+          const resp = snapshot.val()
+          if( resp.tipoUser ) {
+            window.location = "admin.html";
+          } else {
+            window.location = "preguntas.html";
+          }
+      });
 
-      } else {
-        console.log("no logeado");
-      }
-    });
-  }
-  function logout(params) {
-    firebase.auth().signOut().then(function() {
-       console.log("se salio el perro");
-    }).catch(function(error) {
-      // An error happened.
-    });
-  }
-  session();
-  
- 
- 
+      
+    }
+  });
+}
+
+function logout(params) {
+  firebase.auth().signOut().then(function () {
+    window.location = 'index.html';
+    localStorage.clear();
+  }).catch(function (error) {
+    // An error happened.
+  });
+}
+// session();
+
+
